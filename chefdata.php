f83621466ffa45123371076cb0cb6f3f4eb85588
yamikjg4 <yamikgandhi@outlook.com>
2021-10-02 16:44:09 +0530

    new

--- chefdata.php
+++ chefdata.php
@@ -45,7 +45,7 @@
         </div>
         </div>
     </section>
-    <section class="py-5">
+    <section class="py-5 animate__animated animate__fadeInLeft animate__delay-30s">
            <div class="container">
              <!-- row -->
              <div class="row">
@@ -63,7 +63,7 @@
    </span>
 </div>
 <?php
-$sql="SELECT * FROM food JOIN category ON category.cat_id=food.Cat_id WHERE Chef_id=$id GROUP By category.category_name";
+$sql="SELECT * FROM food JOIN category ON category.cat_id=food.Cat_id GROUP By category.category_name";
 $exe=mysqli_query($con,$sql);
 while($result=mysqli_fetch_array($exe)){
 ?>
@@ -93,8 +93,10 @@ while($result=mysqli_fetch_array($exe)){
                             ?>
                        <div class="col-md-4 col-6">
                        <div class="card">
-                <img class="card-img-top img-fluid" src="<?php echo $resp["Food_Image"]; ?>" alt="" style="width:300px;height:300px;background-size:cover;">
-                <div class="card-body text-center">
+                         <div class="inner">
+              <img class="card-img-top img-fluid" src="<?php echo $resp["Food_Image"]; ?>" alt="" style="width:300px;height:300px;background-size:cover;">
+              </div> 
+              <div class="card-body text-center">
                   <h5 class="card-title text-center"><?php echo $resp["Food_Name"]; ?></h5>
                   <a href="detail.php?id=<?php echo $resp["Food_id"];?>" class="btn btn-info">Info Chef</a>
                   <!-- <p class="card-text">Text</p> -->
@@ -128,7 +130,7 @@ while($result=mysqli_fetch_array($exe)){
 </section>
     <?php include('template/footer.php');?>
 </body>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></scrip>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <script src="script/script.js"></script>
